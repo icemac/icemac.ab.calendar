c2902697858804fcf6f0c8435c74bf67616be0f5
Michael Howitz <mh@gocept.com>
2013-01-02 22:10:40 +0100

    function expects an event object to be called with

--- src/icemac/ab/calendar/generations/install.py
+++ src/icemac/ab/calendar/generations/install.py
@@ -5,10 +5,12 @@
 
 
 import icemac.ab.calendar.install
+import icemac.addressbook.addressbook
 import icemac.addressbook.generations.utils
 
 
 @icemac.addressbook.generations.utils.evolve_addressbooks
 def evolve(address_book):
     """Installs the calendar into each existing address book."""
-    icemac.ab.calendar.install.install_calendar(address_book)
+    icemac.ab.calendar.install.install_calendar(
+        icemac.addressbook.addressbook.AddressBookCreated(address_book))
