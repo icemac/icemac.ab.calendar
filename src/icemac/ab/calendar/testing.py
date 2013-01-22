7009f661ece5fab812e677c0dc10b861b3f0ce76
Michael Howitz <mh@gocept.com>
2013-01-22 18:12:29 +0100

    refactored test object creation

--- src/icemac/ab/calendar/testing.py
+++ src/icemac/ab/calendar/testing.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 import icemac.ab.calendar
 import icemac.ab.calendar.category
+import icemac.ab.calendar.event
 import icemac.addressbook.testing
 import icemac.addressbook.utils
 import unittest2 as unittest
@@ -16,28 +17,40 @@ TEST_BROWSER_LAYER = icemac.addressbook.testing.TestBrowserLayer(
     'Calendar', ZODB_LAYER)
 
 
-class TestMixIn(object):
-    """Mix-in methods for test cases."""
+class ZODBTestMixIn(object):
+    """Mix-in methods for test cases using the ZODB."""
+
+    def _create(self, class_, parent=None, **kw):
+        """Helper method to create an object in the ZODB."""
+        ab = self.layer['addressbook']
+        if parent is None:
+            parent = ab
+        else:
+            parent = getattr(ab, parent)
+        name = icemac.addressbook.utils.create_and_add(class_, **kw)
+        return parent[name]
 
     def create_category(self, title):
         """Create a new event category."""
-        ab = self.layer['addressbook']
-        parent = ab.calendar_categories
-        name = icemac.addressbook.utils.create_and_add(
-            parent, icemac.ab.calendar.category.Category, title=title)
-        return parent[name]
+        return self._create(icemac.ab.calendar.category.Category,
+                            parent='calendar_categories', title=title)
+
+    def create_event(self, **kw):
+        """Create a new event in the calendar."""
+        return self._create(icemac.ab.calendar.event.Event,
+                            parent='calendar', **kw)
 
 
-class ZCMLTestCase(unittest.TestCase, TestMixIn):
+class ZCMLTestCase(unittest.TestCase):
     """Test case for test which only need the ZCML registrations."""
     layer = ZCML_LAYER
 
 
-class ZODBTestCase(unittest.TestCase, TestMixIn):
+class ZODBTestCase(unittest.TestCase, ZODBTestMixIn):
     """Test case for test which need the ZODB."""
     layer = ZODB_LAYER
 
 
-class BrowserTestCase(unittest.TestCase, TestMixIn):
+class BrowserTestCase(unittest.TestCase, ZODBTestMixIn):
     """Test case for browser tests."""
     layer = TEST_BROWSER_LAYER
