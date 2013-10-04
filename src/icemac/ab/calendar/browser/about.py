d593e46a2118462c1e5dedbafc5a5322b601bca0
Michael Howitz <mh@gocept.com>
2013-10-04 12:04:06 +0200

    Display icon copyright on about page

---
+++ src/icemac/ab/calendar/browser/about.py
@@ -0,0 +1,7 @@
+import zope.interface
+import icemac.addressbook.browser.about.about
+
+
+class CalendarIcon(icemac.addressbook.browser.about.about.IconProviderInfo):
+    name = u'GraphicsFuel'
+    url = u'http://www.graphicsfuel.com/2012/02/calendar-icon-psd'
