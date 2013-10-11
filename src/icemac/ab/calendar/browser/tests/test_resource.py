5a579c88ff4613ac3a385d097068967ffc32a3fc
Michael Howitz <mh@gocept.com>
2013-10-11 21:55:07 +0200

    Fix test after renaming Fanstatic library

--- src/icemac/ab/calendar/browser/tests/test_resource.py
+++ src/icemac/ab/calendar/browser/tests/test_resource.py
@@ -6,13 +6,11 @@ class ResourceTests(icemac.ab.calendar.testing.BrowserTestCase):
 
     def test_calender_view_renders_calender_css(self):
         browser = self.get_browser('cal-visitor')
-        browser.handleErrors = False
-
         browser.open('http://localhost/ab/++attribute++calendar')
-        self.assertIn('calendar_css/calendar.css', browser.contents)
+        self.assertIn('/fanstatic/calendar/calendar.css', browser.contents)
 
     def test_event_add_form_renders_calender_css(self):
         browser = self.get_browser('cal-editor')
         browser.open(
             'http://localhost/ab/++attribute++calendar/@@addEvent.html')
-        self.assertIn('calendar_css/calendar.css', browser.contents)
+        self.assertIn('/fanstatic/calendar/calendar.css', browser.contents)
