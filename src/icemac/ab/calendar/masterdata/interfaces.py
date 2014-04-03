d1b679e3f46fcb638254a5ecc89e02f854a8ed4c
Michael Howitz <mh@gocept.com>
2014-04-03 16:14:49 +0200

    Move interface to an interfaces.py module.

---
+++ src/icemac/ab/calendar/masterdata/interfaces.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2013-2014 Michael Howitz
+# See also LICENSE.txt
+import z3c.menu.ready2go
+
+
+class ICalendarMasterData(z3c.menu.ready2go.ISiteMenu):
+    """Menu containing links to edit calendar master data."""
