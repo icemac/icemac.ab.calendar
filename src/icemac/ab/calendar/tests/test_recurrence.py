1d02806fae531985ba01dd74fd2a5c03b4e8185a
Michael Howitz <mh@gocept.com>
2014-07-22 16:10:17 +0200

    Refactor for reusability.

--- src/icemac/ab/calendar/tests/test_recurrence.py
+++ src/icemac/ab/calendar/tests/test_recurrence.py
@@ -1,11 +1,11 @@
 import icemac.ab.calendar.testing
 
 
-class RecurrenceTests(icemac.ab.calendar.testing.ZCMLTestCase):
-    """Testing ..recurrence.*"""
+class RecurrenceMixIn(object):
+    """Mix-in to test ..recurrence.*."""
 
     def setUp(self):
-        super(RecurrenceTests, self).setUp()
+        super(RecurrenceMixIn, self).setUp()
         self.recurrence_start = self.get_datetime((2013, 5, 3, 21, 45))
         self.interval_start = self.get_datetime((2014, 4, 1))
         self.interval_end = self.get_datetime((2014, 4, 30))
@@ -17,13 +17,17 @@ class RecurrenceTests(icemac.ab.calendar.testing.ZCMLTestCase):
         end = self.interval_end if end is None else end
         return list(get_recurrences(datetime, adapter_name, start, end))
 
-    def test_Weekly_fulfills_IRecurringDateTime_interface(self):
+
+class WeeklyTests(RecurrenceMixIn, icemac.ab.calendar.testing.ZCMLTestCase):
+    """Testing ..recurrence.Weekly"""
+
+    def test_instance_fulfills_IRecurringDateTime_interface(self):
         from zope.interface.verify import verifyObject
         from ..interfaces import IRecurringDateTime
         from ..recurrence import Weekly
         self.assertTrue(verifyObject(IRecurringDateTime, Weekly(None)))
 
-    def test_weekly_returns_all_dates_in_interval_for_same_weekday(self):
+    def test_returns_all_dates_in_interval_for_same_weekday(self):
         self.assertEqual(
             [self.get_datetime((2014, 4, 4, 21, 45)),
              self.get_datetime((2014, 4, 11, 21, 45)),
@@ -32,7 +36,7 @@ class RecurrenceTests(icemac.ab.calendar.testing.ZCMLTestCase):
         self.assertEqual(self.recurrence_start.isoweekday(),
                          self.callFUT('weekly')[0].isoweekday())
 
-    def test_weekly_does_not_start_before_datetime(self):
+    def test_does_not_start_before_datetime(self):
         self.assertEqual(
             [self.get_datetime((2014, 4, 18, 21, 45)),
              self.get_datetime((2014, 4, 25, 21, 45))],
@@ -41,21 +45,18 @@ class RecurrenceTests(icemac.ab.calendar.testing.ZCMLTestCase):
         self.assertEqual(self.recurrence_start.isoweekday(),
                          self.callFUT('weekly')[0].isoweekday())
 
-    def test_weekly_returns_empty_interval_if_datetime_after_interval_end(
-            self):
+    def test_returns_empty_interval_if_datetime_after_interval_end(self):
         self.assertEqual(
-            [],
-            self.callFUT('weekly',
-                         datetime=self.get_datetime((2014, 5, 1, 21, 45))))
+            [], self.callFUT(
+                'weekly', datetime=self.get_datetime((2014, 5, 1, 21, 45))))
 
-    def test_weekly_interval_end_does_not_belong_to_interval(self):
+    def test_interval_end_does_not_belong_to_interval(self):
         self.assertEqual(
-            [],
-            self.callFUT(
+            [], self.callFUT(
                 'weekly', start=self.get_datetime((2014, 4, 24)),
                 end=self.get_datetime((2014, 4, 25))))
 
-    def test_weekly_interval_start_belongs_to_interval(self):
+    def test_interval_start_belongs_to_interval(self):
         self.assertEqual(
             [self.get_datetime((2014, 4, 4, 21, 45))],
             self.callFUT(
