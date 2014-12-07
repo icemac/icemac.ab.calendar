488ff70787def3bf15ea3fe9b23a2b8caa9be713
Michael Howitz <mh@gocept.com>
2014-12-07 13:11:38 +0100

    Snapshot styling

--- src/icemac/ab/calendar/browser/resources/calendar.js
+++ src/icemac/ab/calendar/browser/resources/calendar.js
@@ -2,4 +2,5 @@
     $('#calendar #calendar-select-form select').on('change', function() {
         $('#calendar #calendar-select-form #form-buttons-apply').click();
     });
+    $('#calendar .body .border').height($('#calendar .body table').height());
 })(jQuery);
