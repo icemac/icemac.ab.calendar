8ea5b6c25af9fb1e2af797660751c90eb1cd303f
Michael Howitz <mh@gocept.com>
2014-12-09 21:54:14 +0100

    Make redering nicer:
    - Resize borders on window resize.
    - Better display of calendar.
    - Re-enable access to the year listing. (Was hidden behind other div container.)

--- src/icemac/ab/calendar/browser/resources/calendar.js
+++ src/icemac/ab/calendar/browser/resources/calendar.js
@@ -1,6 +1,12 @@
 (function($) {
+    // Auto submit month and year drop downs:
     $('#calendar #calendar-select-form select').on('change', function() {
         $('#calendar #calendar-select-form #form-buttons-apply').click();
     });
-    $('#calendar .body .border').height($('#calendar .body table').height());
+    // Make length of border divs equal to center table:
+    $(window).resize(function() {
+        $('#calendar .body .border').height(
+            $('#calendar .body .table').height());
+    });
+    $(window).trigger('resize'); // Initially adapt size.
 })(jQuery);
