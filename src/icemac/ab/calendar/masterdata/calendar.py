c3a019c22311e36e87c8544014b6d07ac08a93cc
Michael Howitz <mh@gocept.com>
2015-05-22 09:03:52 +0200

    Add specific metadata form and label.
    The metadata form does not contain editor and creation date because editor
    is always empty and the creation date of the annotation does not matter.

--- src/icemac/ab/calendar/masterdata/calendar.py
+++ src/icemac/ab/calendar/masterdata/calendar.py
@@ -1,13 +1,33 @@
+from icemac.addressbook.i18n import _
 import grokcore.component as grok
 import icemac.ab.calendar.interfaces
 import icemac.addressbook.browser.base
+import icemac.addressbook.browser.metadata
+import icemac.addressbook.metadata.interfaces
+import z3c.form.field
+import zope.dublincore.interfaces
 import zope.schema.interfaces
+
+
+class ModifierMetadataGroup(
+        icemac.addressbook.browser.metadata.MetadataBaseGroup):
+    """Group displaying only meta data information of modifier.
+
+    To be used when creator is empty and creation date is irrelevant.
+    """
+
+    fields = z3c.form.field.Fields(
+        icemac.addressbook.metadata.interfaces.IEditor).select('modifier')
+    fields += z3c.form.field.Fields(
+        zope.dublincore.interfaces.IDCTimes).select('modified')
 
 
 class CalendarView(icemac.addressbook.browser.base.GroupEditForm):
 
     """Edit the calendar view settings."""
 
+    label = _(u'Edit calendar display settings')
+    groups = (ModifierMetadataGroup,)
     interface = icemac.ab.calendar.interfaces.ICalendarDisplaySettings
     next_url = 'parent'
 
