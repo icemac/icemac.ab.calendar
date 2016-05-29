3f4b0595c25bc7c05384570e5d44f4c270ccddb8
Michael Howitz <mh@gocept.com>
2016-05-29 16:05:48 +0200

    Clean-up and update to icemac.addressbook 2.7.x

--- src/icemac/ab/calendar/browser/tests/test_resource.py
+++ src/icemac/ab/calendar/browser/tests/test_resource.py
@@ -1,12 +1,12 @@
-def test_resource__1(address_book, browser):
-    """calender_view_renders_calender_css."""
-    browser.login('cal-visitor')
-    browser.open(browser.CALENDAR_MONTH_OVERVIEW_URL)
-    assert '/fanstatic/calendar/calendar.css' in browser.contents
+import pytest
 
 
-def test_resource__2(address_book, browser):
-    """event_add_form_renders_calender_css."""
-    browser.login('cal-editor')
-    browser.open(browser.EVENT_ADD_URL)
-    assert '/fanstatic/calendar/calendar.css' in browser.contents
+@pytest.mark.parametrize('username,url', [
+    ('cal-visitor', 'CALENDAR_MONTH_OVERVIEW_URL'),
+    ('cal-editor', 'EVENT_ADD_URL')])
+def test_resource__1(address_book, browser, username, url):
+    """The `calendar.css` is rendered on calendar pages."""
+    browser.login(username)
+    browser.open(getattr(browser, url))
+    assert 'href="/fanstatic/calendar/:version:' in browser.contents
+    assert 'calendar.css' in browser.contents
