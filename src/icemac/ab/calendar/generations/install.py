4d42db4b2d8403c9330038c75e2c4b4c35a49fb6
Michael Howitz <mh@gocept.com>
2017-04-30 14:27:06 +0200

    PEP-257.

--- src/icemac/ab/calendar/generations/install.py
+++ src/icemac/ab/calendar/generations/install.py
@@ -1,6 +1,3 @@
-"""Initial generation."""
-
-
 import icemac.ab.calendar.install
 import icemac.addressbook.addressbook
 import icemac.addressbook.generations.utils
@@ -8,6 +5,6 @@ import icemac.addressbook.generations.utils
 
 @icemac.addressbook.generations.utils.evolve_addressbooks
 def evolve(address_book):
-    """Installs the calendar into each existing address book."""
+    """Install the calendar into each existing address book."""
     icemac.ab.calendar.install.install_calendar(
         icemac.addressbook.addressbook.AddressBookCreated(address_book))
