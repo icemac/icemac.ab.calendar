fce0c18fafa4ffbcc2e23c99fe01f5d6b0115448
Michael Howitz <mh@gocept.com>
2018-02-24 22:14:52 +0100

    Fix JS syntax.

--- src/icemac/ab/calendar/browser/resources/eventview.js
+++ src/icemac/ab/calendar/browser/resources/eventview.js
@@ -1,10 +1,9 @@
 $(function () {
     // activate popover on close button
-    $('[data-toggle="popover"]').popover()
+    $('[data-toggle="popover"]').popover();
 
     // Auto submit event view dropdown
     $('#eventview select#views').on('change', function() {
         $('#eventview form').submit();
     });
-})
-
+});
