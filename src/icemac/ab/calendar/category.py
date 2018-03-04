07ffd6298620a251f7c2d7d3da0129fd1d2bf69e
Michael Howitz <mh@gocept.com>
2018-03-04 20:20:26 +0100

    Allow to change a category title and be able to find events for this category afterwards.

--- src/icemac/ab/calendar/category.py
+++ src/icemac/ab/calendar/category.py
@@ -1,10 +1,13 @@
 from icemac.addressbook.i18n import _
+import grokcore.component as grok
 import icemac.ab.calendar.interfaces
 import icemac.addressbook.utils
 import persistent
+import zope.catalog.interfaces
 import zope.container.btree
 import zope.container.contained
 import zope.interface
+import zope.lifecycleevent
 import zope.schema.fieldproperty
 
 
@@ -29,3 +32,21 @@ class Category(persistent.Persistent,
 
 unique_titles = icemac.addressbook.utils.unique_by_attr_factory(
     'title', _(u'This category already exists.'))
+
+
+@grok.subscribe(icemac.ab.calendar.interfaces.ICategory,
+                zope.lifecycleevent.IObjectModifiedEvent)
+def changed(obj, event):
+    """Update the catalog index if a category's title has changed."""
+    # The notation of of this function is meant to have no code branches
+    # because this cannot be easily tested and the other branches do not get
+    # excecuted in real live.
+    changed = [
+        desc
+        for desc in event.descriptions
+        if (desc.interface == icemac.ab.calendar.interfaces.ICategory
+            and 'title' in desc.attributes)]
+    for x in changed:
+        catalog = zope.component.getUtility(
+            zope.catalog.interfaces.ICatalog)
+        catalog.updateIndex(catalog.get('keywords'))
--- src/icemac/ab/calendar/tests/test_category.py
+++ src/icemac/ab/calendar/tests/test_category.py
@@ -2,6 +2,7 @@
 from icemac.ab.calendar.category import Category, CategoryContainer
 from icemac.ab.calendar.interfaces import ICategory, ICategories
 from zope.interface.verify import verifyObject
+import zope.component.hooks
 
 
 def test_category__Category__1():
@@ -20,3 +21,35 @@ def test_category__Category____repr____1():
 def test_category__CategoryContainer__1():
     """It implements the `ICategories` interface."""
     assert verifyObject(ICategories, CategoryContainer())
+
+
+def test_category__changed__1(
+        address_book, CategoryFactory, EventFactory, DateTime, browser):
+    """It updates the `keywords` index if a category title was changed."""
+    cat = CategoryFactory(address_book, u'foo')
+    EventFactory(address_book, category=cat, alternative_title=u'event',
+                 datetime=DateTime.now)
+
+    event_titles = [
+        x.alternative_title
+        for x in address_book.calendar.get_events(
+            DateTime.add(DateTime.now, days=-1),
+            DateTime.add(DateTime.now, days=+1),
+            categories=[u'foo'],
+        )]
+    assert ['event'] == event_titles
+
+    browser.login('mgr')
+    browser.open(browser.CALENDAR_CATEGORY_EDIT_URL)
+    browser.getControl('event category').value = 'bar'
+    browser.getControl('Apply').click()
+
+    with zope.component.hooks.site(address_book):
+        event_titles = [
+            x.alternative_title
+            for x in address_book.calendar.get_events(
+                DateTime.add(DateTime.now, days=-1),
+                DateTime.add(DateTime.now, days=+1),
+                categories=['bar'],
+            )]
+    assert ['event'] == event_titles
